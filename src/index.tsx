8e9798f35721a95631300b842e69d2fd60c5e1c9
Gary Guo <nbdd0121@hotmail.com>
2017-02-11 15:53:35 +0000

    build: add polyfill url-search-params

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+// Polyfills
 import 'core-js/es6';
+declare function require(name: string): any;
+if (typeof (window as any).URLSearchParams === 'undefined') {
+    (window as any).URLSearchParams = require('url-search-params');
+}
+
 import 'normalize.css';
 import './index.css';
 
