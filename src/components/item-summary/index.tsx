eb52717b2d71bf6b73324f4df864750a06ff835a
Gary Guo <nbdd0121@hotmail.com>
2017-02-11 19:27:45 +0000

    component: implemented component for showing message summary

---
+++ src/components/item-summary/index.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import * as moment from 'moment';
+
+import { Message } from '../../services/api';
+import './index.css';
+
+interface ItemSummaryProps {
+    item: Message;
+    selected?: boolean;
+}
+
+export class ItemSummary extends React.Component<ItemSummaryProps, {}> {
+    getTime() {
+        var m = moment(this.props.item.date);
+        var now = moment(Date.now());
+        if (m.isSame(now, 'day')) {
+            return m.format('HH:mm');
+        }
+        if (m.isSame(now, 'year')) {
+            return m.format('DD MMM');
+        }
+        return m.format('DD/MM/YYYY');
+    }
+
+    render() {
+        let item = this.props.item;
+        return <div className={`ItemSummary top${item.unread ? ' unread' : ''}${this.props.selected ? ' selected' : ''}`}>
+            <div className="ItemSummary contact">{item.from.name || item.from.address || '(no sender)'}</div>
+            <div className="ItemSummary title-line">
+                <span className="ItemSummary title">{item.subject || '(no subject)'}</span>
+                <span className="ItemSummary time">{this.getTime()}</span>
+            </div>
+            <div className="ItemSummary summary">{item.summary}</div>
+        </div>;
+    }
+}
