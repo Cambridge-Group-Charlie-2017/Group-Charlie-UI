0fc477153c7e1ddb02c138111d0296478e00ae21
Gary Guo <nbdd0121@hotmail.com>
2017-02-13 12:35:37 +0000

    Move require declaration to global

--- src/global.d.ts
+++ src/global.d.ts
@@ -38,3 +38,9 @@ declare class URLSearchParams {
      /** Iterator */
     [Symbol.iterator](): IterableIterator<number>;
 }
+
+/*
+ * Some modules we use does not provide typescript definitions,
+ * so we need to import them using require instead of import
+ */
+declare function require(name: string): any;
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from "react-dom";
 
 // Polyfills
 import 'core-js/es6';
-declare function require(name: string): any;
 if (typeof (window as any).URLSearchParams === 'undefined') {
     (window as any).URLSearchParams = require('url-search-params');
 }
